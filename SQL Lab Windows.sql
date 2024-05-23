367f98366c42b7b414a684b8ee08573e0237d5b8
katjg2024 <168486302+katjg2024@users.noreply.github.com>
2024-05-22 23:17:16 -0400

    adjusted lab with corrections needed

--- SQL Lab Windows.sql
+++ SQL Lab Windows.sql
@@ -4,13 +4,14 @@ USE sakila;
 
 SELECT f.title, f.length,
 RANK() OVER(ORDER BY length DESC)
-FROM film AS f;
+FROM film AS f
+WHERE f.length > 0;
 
 -- Rank films by length within the rating category and create an output table that includes the title, length, rating and rank columns only. 
 -- Filter out any rows with null or zero values in the length column.
 
 SELECT f.title, f.length, f.rating,
-RANK() OVER(ORDER BY rating DESC) AS 'rank'
+RANK() OVER(PARTITION BY rating ORDER BY length DESC) AS 'rank'
 FROM film AS f
 WHERE f.length IS NOT NULL AND f.rating !=0;
 
@@ -18,39 +19,33 @@ WHERE f.length IS NOT NULL AND f.rating !=0;
 -- as well as the total number of films in which they have acted. Hint: Use temporary tables, CTEs, or Views when appropiate to 
 -- simplify your queries. 
 
-CREATE TEMPORARY TABLE actors_ranked
-SELECT fa.film_id, f.title, a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS 'Actor Name',
-RANK() OVER(ORDER BY 'Actor Name') AS 'rank'
-FROM film AS f
-JOIN film_actor AS fa
-ON f.film_id = fa.film_id
-JOIN actor AS a
-ON fa.actor_id = a.actor_id;
+-- CREATE TEMPORARY TABLE actors_ranked
+#SELECT fa.film_id, f.title, a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS 'Actor Name',
+#RANK() OVER(ORDER BY 'Actor Name') AS 'rank'
+#FROM film AS f
+#JOIN film_actor AS fa
+#ON f.film_id = fa.film_id
+#JOIN actor AS a
+#ON fa.actor_id = a.actor_id;
 
 SELECT *
 FROM actors_ranked;
 
--- helped by bot
-WITH actor_film_counts AS (
-    SELECT 
+-- CORRECTION BY IRONBOT
+WITH ranked_actors AS (
+    SELECT
         fa.actor_id,
-        COUNT(*) AS num_films
+        a.first_name,
+        a.last_name,
+        COUNT(*) AS film_count,
+        RANK() OVER (ORDER BY COUNT(*) DESC) AS 'rank'
     FROM film_actor AS fa
+    JOIN actor AS a ON fa.actor_id = a.actor_id
     GROUP BY fa.actor_id
-),
-max_actor_films AS (
-    SELECT 
-        actor_id,
-        num_films,
-        RANK() OVER(ORDER BY num_films DESC) AS actor_rank
-    FROM actor_film_counts
 )
-SELECT 
-    f.title AS film_title,
-    CONCAT(a.first_name, ' ', a.last_name) AS actor_name,
-    maf.num_films AS num_films_acted
-FROM 
-    film_actor AS fa
-JOIN film AS f ON fa.film_id = f.film_id
+SELECT f.title as film_title, CONCAT(a.first_name, ' ', a.last_name) as actor_name, ra.film_count
+FROM film AS f
+JOIN film_actor AS fa ON f.film_id = fa.film_id
 JOIN actor AS a ON fa.actor_id = a.actor_id
-JOIN max_actor_films AS maf ON a.actor_id = maf.actor_id AND maf.actor_rank = 1;
+JOIN ranked_actors AS ra ON ra.actor_id = a.actor_id
+WHERE ra.rank = 1;
